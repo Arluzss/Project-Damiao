b70e680941dbedd2eb138ab345803c7cede093a3
Leonel <viniciusleonel1444@gmail.com>
2025-12-23 15:30:44 -0300

    feat: página de microempreendedores

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { Register } from './pages/Register';
 import Login from './pages/Login';
 import { Routes, Route } from "react-router-dom";
+import { Entrepreneurs } from "./pages/Entrepreneurs";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/registro" element={<Register/>} />
         <Route path= "/entrar" element={<Login/>} />  
+        <Route path= "/microempreendedores" element={<Entrepreneurs/>} /> 
          
       </Routes>
     </>
