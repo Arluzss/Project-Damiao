ac71e8462b8f5921b1f3f1845d66ae73aeac8842
Leonel <viniciusleonel1444@gmail.com>
2025-12-23 10:06:09 -0300

    feat: listagem de cursos disponíveis

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import { Home } from "./pages/Home";
 import { Register } from './pages/Register';
 import { Routes, Route } from "react-router-dom";
+import { Courses } from "./pages/Courses";
+
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
          <Route path="/registro" element={<Register/>} />
+         <Route path="/cursos" element= {<Courses/>} />
+
       </Routes>
     </>
   );
