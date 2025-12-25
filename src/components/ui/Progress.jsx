78e8ef6dd46ea15c9fb0123eccf642cd36782017
Leonel <viniciusleonel1444@gmail.com>
2025-12-25 12:51:39 -0300

    feat : adição de imports : Calendar, Carousel, Progress e HoverCard ”

---
+++ src/components/ui/Progress.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+import { cn } from "./utils";
+import "./Progress.css";
+
+function Progress({ className, value, ...props }) {
+  return (
+    <ProgressPrimitive.Root
+      data-slot="progress"
+      className={cn("progress-root", className)}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        data-slot="progress-indicator"
+        className="progress-indicator"
+        style={{
+          transform: `translateX(-${100 - (value || 0)}%)`,
+        }}
+      />
+    </ProgressPrimitive.Root>
+  );
+}
+
+export { Progress };
