2f317503ab52e3ce07c40bf319c8609e806fd5cd
Arluzss <arleynunes251@gmail.com>
2025-12-29 22:51:31 -0300

    feat: atualizar enum TipoPessoa e habilitar CORS no app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const authRoutes = require('./routes/AuthRoutes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
 
-module.exports = app;
+module.exports = app; 
